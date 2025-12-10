ddf8e28dfded654b24a14d912cd17999c5a96e1a
anari-lab <andrea.ferrari@itiscannizzarocolleferro.it>
2025-12-10 12:40:53 +0100

    Main

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ public class Main {
     //calcolo sconto in acquirente
     static Magazzino magazzino=new Magazzino();
     static Frame01 frame= new Frame01(magazzino);
-    static void main(String[] args) {
+    public static void main(String[] args) {
     }
 }
